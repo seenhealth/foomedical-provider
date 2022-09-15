0e841b4f209cd6089810f5f17eebf31c2aa0ab89
Cody Ebberson <cody@ebberson.com>
2022-09-15 16:47:45 -0700

    Forms

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@ import { UserConfiguration } from '@medplum/fhirtypes';
 import { ErrorBoundary, Header, Loading, useMedplum } from '@medplum/react';
 import React, { Suspense } from 'react';
 import { Route, Routes, useNavigate } from 'react-router-dom';
+import { Slide, ToastContainer } from 'react-toastify';
+import { FormsList } from './pages/FormsList';
 import { HomePage } from './pages/HomePage';
 import { LandingPage } from './pages/LandingPage';
 import { PatientPage } from './pages/PatientPage';
 import { PatientsList } from './pages/PatientsList';
 import { ProfilePage } from './pages/ProfilePage';
+import { QuestionnairePage } from './pages/QuestionnairePage';
 import { ResourcePage } from './pages/ResourcePage';
+import { SchedulePage } from './pages/SchedulePage';
 import { SignInPage } from './pages/SignInPage';
 import { TaskPage } from './pages/TaskPage';
+import 'react-toastify/dist/ReactToastify.css';
 
 export function App(): JSX.Element | null {
   const navigate = useNavigate();
@@ -41,6 +46,15 @@ export function App(): JSX.Element | null {
 
   return (
     <>
+      <ToastContainer
+        position="top-right"
+        transition={Slide}
+        autoClose={3000}
+        hideProgressBar
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       {profile && (
         <Header
           bgColor="#202020"
@@ -61,8 +75,12 @@ export function App(): JSX.Element | null {
             <Route path="/signin" element={<SignInPage />} />
             <Route path="/profile" element={<ProfilePage />} />
             <Route path="/patients" element={<PatientsList />} />
+            <Route path="/visits" element={<SchedulePage />} />
+            <Route path="/forms" element={<FormsList />} />
             <Route path="/Patient/:id/:tab" element={<PatientPage />} />
             <Route path="/Patient/:id" element={<PatientPage />} />
+            <Route path="/Questionnaire/:id/:tab" element={<QuestionnairePage />} />
+            <Route path="/Questionnaire/:id" element={<QuestionnairePage />} />
             <Route path="/Task/:id" element={<TaskPage />} />
             <Route path="/:resourceType/:id" element={<ResourcePage />} />
           </Routes>
---
+++ src/pages/QuestionnairePage.tsx
@@ -0,0 +1,71 @@
+import { normalizeErrorString } from '@medplum/core';
+import { Questionnaire } from '@medplum/fhirtypes';
+import {
+  Avatar,
+  Document,
+  QuestionnaireBuilder,
+  QuestionnaireForm,
+  Scrollable,
+  Tab,
+  TabList,
+  TabPanel,
+  TabSwitch,
+  useMedplum,
+} from '@medplum/react';
+import React from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import './PatientHeader.css';
+import './PatientPage.css';
+
+export function QuestionnairePage(): JSX.Element {
+  const navigate = useNavigate();
+  const { id, tab } = useParams() as {
+    id: string;
+    tab: string;
+  };
+  const medplum = useMedplum();
+  const questionnaire = medplum.readResource('Questionnaire', id).read() as Questionnaire;
+  const defaultTab = 'preview';
+
+  function onSubmit(newResource: Questionnaire): void {
+    medplum
+      .updateResource(newResource)
+      .then(() => medplum.get<Questionnaire>(medplum.fhirUrl('Questionnaire', id), { cache: 'reload' }))
+      .then(() => toast.success('Success'))
+      .catch((err) => toast.error(normalizeErrorString(err)));
+  }
+
+  return (
+    <>
+      <Scrollable className="medplum-surface" height={100}>
+        <div className="medplum-patient-header">
+          <Avatar alt="Q" size="large" color="#888" />
+          <div className="medplum-patient-header-details">
+            <div>
+              <strong>Title:</strong> {questionnaire.title}
+            </div>
+            <div>
+              <strong>Publisher:</strong> {questionnaire.publisher}
+            </div>
+          </div>
+        </div>
+      </Scrollable>
+      <TabList value={tab || defaultTab} onChange={(newTab) => navigate(`/Questionnaire/${id}/${newTab}`)}>
+        <Tab name="preview" label="Preview" />
+        <Tab name="editor" label="Editor" />
+      </TabList>
+      <Document>
+        <TabSwitch value={tab || defaultTab}>
+          <TabPanel name="preview">
+            <QuestionnaireForm questionnaire={questionnaire} onSubmit={() => alert('You submitted the preview')} />
+          </TabPanel>
+          <TabPanel name="editor">
+            <h2>Editor</h2>
+            <QuestionnaireBuilder questionnaire={questionnaire} onSubmit={onSubmit} />
+          </TabPanel>
+        </TabSwitch>
+      </Document>
+    </>
+  );
+}
