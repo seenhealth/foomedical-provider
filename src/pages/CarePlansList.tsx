aa0a0ef36b2cf8e0e613d72931e432350e9c63de
Cody Ebberson <cody@ebberson.com>
2022-09-18 12:03:31 -0700

    Missing files

---
+++ src/pages/CarePlansList.tsx
@@ -0,0 +1,44 @@
+import { formatSearchQuery, parseSearchDefinition, SearchRequest } from '@medplum/core';
+import { Button, Document, Loading, MemoizedSearchControl, useMedplum } from '@medplum/react';
+import React, { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+export function CarePlansList(): JSX.Element {
+  const medplum = useMedplum();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [search, setSearch] = useState<SearchRequest>();
+
+  useEffect(() => {
+    const newSearch = parseSearchDefinition('PlanDefinition?' + location.search);
+    newSearch.fields = ['title', 'publisher', '_lastUpdated'];
+    setSearch(newSearch);
+  }, [medplum, location]);
+
+  if (!search) {
+    return <Loading />;
+  }
+
+  return (
+    <Document>
+      <h2>Care Plans</h2>
+      <MemoizedSearchControl
+        hideToolbar={true}
+        hideFilters={true}
+        checkboxesEnabled={false}
+        search={search}
+        userConfig={medplum.getUserConfiguration()}
+        onClick={(e) => navigate(`/${e.resource.resourceType}/${e.resource.id}`)}
+        onAuxClick={(e) => window.open(`/${e.resource.resourceType}/${e.resource.id}`, '_blank')}
+        onChange={(e) => {
+          navigate(`/${search.resourceType}${formatSearchQuery(e.definition)}`);
+        }}
+        onNew={() => navigate(`/${search.resourceType}/new`)}
+      />
+      <div>
+        <Button primary={true}>New</Button>
+        <Button primary={true}>Import</Button>
+      </div>
+    </Document>
+  );
+}
---
+++ src/pages/PlanDefinitionPage.tsx
@@ -0,0 +1,70 @@
+import { normalizeErrorString } from '@medplum/core';
+import { PlanDefinition } from '@medplum/fhirtypes';
+import {
+  Avatar,
+  Document,
+  PlanDefinitionBuilder,
+  ResourceTable,
+  Scrollable,
+  Tab,
+  TabList,
+  TabPanel,
+  TabSwitch,
+  useMedplum,
+} from '@medplum/react';
+import React from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import './PatientHeader.css';
+import './PatientPage.css';
+
+export function PlanDefinitionPage(): JSX.Element {
+  const navigate = useNavigate();
+  const { id, tab } = useParams() as {
+    id: string;
+    tab: string;
+  };
+  const medplum = useMedplum();
+  const planDefinition = medplum.readResource('PlanDefinition', id).read() as PlanDefinition;
+  const defaultTab = 'preview';
+
+  function onSubmit(newResource: PlanDefinition): void {
+    medplum
+      .updateResource(newResource)
+      .then(() => toast.success('Success'))
+      .catch((err) => toast.error(normalizeErrorString(err)));
+  }
+
+  return (
+    <>
+      <Scrollable className="medplum-surface" height={100}>
+        <div className="medplum-patient-header">
+          <Avatar alt="C P" size="large" color="#888" />
+          <div className="medplum-patient-header-details">
+            <div>
+              <strong>Title:</strong> {planDefinition.title}
+            </div>
+            <div>
+              <strong>Publisher:</strong> {planDefinition.publisher}
+            </div>
+          </div>
+        </div>
+      </Scrollable>
+      <TabList value={tab || defaultTab} onChange={(newTab) => navigate(`/PlanDefinition/${id}/${newTab}`)}>
+        <Tab name="preview" label="Preview" />
+        <Tab name="editor" label="Editor" />
+      </TabList>
+      <Document>
+        <TabSwitch value={tab || defaultTab}>
+          <TabPanel name="preview">
+            <ResourceTable value={planDefinition} ignoreMissingValues={true} />
+          </TabPanel>
+          <TabPanel name="editor">
+            <h2>Editor</h2>
+            <PlanDefinitionBuilder value={planDefinition} onSubmit={onSubmit} />
+          </TabPanel>
+        </TabSwitch>
+      </Document>
+    </>
+  );
+}
