136de6a8fda4f362211b0b7a33562602880c3d09
Cody Ebberson <cody@ebberson.com>
2022-09-20 11:17:49 -0700

    Group patients on home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -11,8 +11,27 @@ export function HomePage(): JSX.Element {
   const navigate = useNavigate();
   const medplum = useMedplum();
   const profile = useMedplumProfile() as Practitioner;
-  const tasks = medplum.searchResources('Task').read();
   const [reassignTask, setReassignTask] = useState<Task>();
+
+  // Sort tasks by:
+  // 1) Patient
+  // 2) Priority
+  const tasks = medplum
+    .searchResources('Task')
+    .read()
+    .sort((a, b) => {
+      const aPatient = a.for?.reference || '';
+      const bPatient = b.for?.reference || '';
+      if (aPatient !== bPatient) {
+        return aPatient < bPatient ? -1 : 1;
+      }
+      const aPriority = a.priority || '';
+      const bPriority = b.priority || '';
+      if (aPriority !== bPriority) {
+        return aPriority < bPriority ? -1 : 1;
+      }
+      return 0;
+    });
 
   return (
     <>
@@ -20,10 +39,12 @@ export function HomePage(): JSX.Element {
         <h1>Welcome {formatGivenName(profile.name?.[0] as HumanName)}</h1>
         <table className="foo-table">
           <tbody>
-            {tasks.map((task) => (
-              <tr>
+            {tasks.map((task, taskIndex) => (
+              <tr key={task.id}>
                 <td>
-                  <ResourceBadge value={task.for as Reference<Patient>} />
+                  {(taskIndex === 0 || task.for?.reference !== tasks[taskIndex - 1].for?.reference) && (
+                    <ResourceBadge value={task.for as Reference<Patient>} />
+                  )}
                 </td>
                 <td>{getTaskType(task)}</td>
                 <td>{task.description}</td>
@@ -32,8 +53,13 @@ export function HomePage(): JSX.Element {
                   {task.priority && <StatusBadge status={task.priority as string} />}
                 </td>
                 <td>
-                  {getTaskActions(task).map((action) => (
-                    <Button size="small" primary={action.primary} onClick={() => navigate(action.href)}>
+                  {getTaskActions(task).map((action, actionIndex) => (
+                    <Button
+                      key={`action-${actionIndex}`}
+                      size="small"
+                      primary={action.primary}
+                      onClick={() => navigate(action.href)}
+                    >
                       {action.label}
                     </Button>
                   ))}
